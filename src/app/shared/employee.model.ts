5afdea698ce6c65e6decc95172feedbeb48abe8c
Jefffer <jefre123@hotmail.com>
2019-11-04 14:10:17 -0500

    Employee services changes.

--- src/app/shared/employee.model.ts
+++ src/app/shared/employee.model.ts
@@ -1,2 +1,9 @@
 export class Employee {
+    idEmployee: number;
+    employeeName: string;
+    employeePhone: string;
+    employeePosition: string;
+    fk_ContractType: number;
+    hourlySalary: number;
+    monthlySalary: number;
 }
